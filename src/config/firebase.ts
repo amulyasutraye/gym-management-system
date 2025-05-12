d41fd9ef24f4e4bce4161e354bf91d8d425cc2fc
Sutraye Amulya <149887602+amulyasutraye@users.noreply.github.com>
2025-05-12 20:23:15 +0530

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmTrJE4L6DQ_wHhaOzWbkV8qd8cXIE2aM",
-  authDomain: "gym-main-74954.firebaseapp.com",
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-AUTHDOMAN",
   projectId: "gym-main-74954",
   storageBucket: "gym-main-74954.firebasestorage.app",
-  messagingSenderId: "362043775293",
-  appId: "1:362043775293:web:813fa95a7303a627c54d54",
+  messagingSenderId: "YOUR-MESSAGINGSENDERID",
+  appId: "YOURAPPID",
   measurementId: "G-EZG7MXR3HP"
 };
 
